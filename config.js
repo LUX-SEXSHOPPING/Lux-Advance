da75ab983f2a204ee3538af5bb30abb3996e2a5c
LUX <ajudaasofia@gmail.com>
2026-09-12 13:46:49 -0300

    Update config.js

--- config.js
+++ config.js
@@ -10,46 +10,38 @@ const LUX_SUPABASE_ANON_KEY =
 
 
 /* =========================================================
-   VERIFICAÇÃO DA BIBLIOTECA
+   VERIFICAÇÃO DA BIBLIOTECA SUPABASE
    ========================================================= */
 
 if (!window.supabase) {
-
   console.error(
     "Biblioteca Supabase não foi carregada."
   );
-
 }
 
 
 /* =========================================================
-   CLIENTE SUPABASE
+   CRIAR CLIENTE SUPABASE
    ========================================================= */
 
 const luxSupabase =
-  (
-    window.supabase &&
-    LUX_SUPABASE_URL.startsWith("https://") &&
-    LUX_SUPABASE_ANON_KEY.startsWith("sb_publishable_")
-  )
-
+  window.supabase
     ? window.supabase.createClient(
         LUX_SUPABASE_URL,
         LUX_SUPABASE_ANON_KEY
       )
-
     : null;
 
 
 /* =========================================================
-   DISPONIBILIZA O CLIENTE PARA O LUX
+   DISPONIBILIZAR PARA O AUTH.JS
    ========================================================= */
 
 window.luxSupabase = luxSupabase;
 
 
 /* =========================================================
-   E-MAIL ADMINISTRADOR
+   E-MAIL DO ADMINISTRADOR
    ========================================================= */
 
 const LUX_ADMIN_EMAIL =

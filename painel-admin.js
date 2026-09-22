e99d40e4ad93361c61b4697aa22501e9c2d1aae3
LUX <ajudaasofia@gmail.com>
2026-09-22 15:49:38 -0300

    Update painel-admin.js

--- painel-admin.js
+++ painel-admin.js
@@ -36,8 +36,10 @@ function obterSupabase() {
     ) {
         return window.supabase;
     }
-
-    return null;
+   
+ return null;
+}
+  
 // =============================================================
 // UTILITÁRIOS
 // =============================================================
